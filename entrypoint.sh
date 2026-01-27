6e212ad5276dc742a368f5ce4cb92a4e1b2a42f9
Rahul Lohra <rahullohra987@gmail.com>
2026-01-27 13:32:29 +0530

    ent

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,3 @@
-# #!/bin/sh
-
-# # Exit immediately if a command exits with a non-zero status
-# set -e
-
-# # Apply database migrations
-# echo "Applying database migrations..."
-# python manage.py migrate --noinput --settings=config.settings.production
-
-# # Collect static files
-# echo "Collecting static files..."
-# python manage.py collectstatic --noinput --settings=config.settings.production
-
-# # Start Gunicorn
-# echo "Starting Gunicorn..."
-# exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
-
-
 #!/bin/sh
 
 # Exit immediately if a command exits with a non-zero status
@@ -25,11 +7,14 @@ set -e
 echo "Applying database migrations..."
 python manage.py migrate --noinput --settings=config.settings.production
 
+
 # Create superuser ONLY if env vars exist
 if [ "$DJANGO_SU_NAME" ]; then
   echo "Creating superuser (if not exists)..."
   python scripts/create_superuser.py
 fi
+
+
 
 # Collect static files
 echo "Collecting static files..."
@@ -38,3 +23,23 @@ python manage.py collectstatic --noinput --settings=config.settings.production
 # Start Gunicorn
 echo "Starting Gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+
+
+# #!/bin/sh
+
+# # Exit immediately if a command exits with a non-zero status
+# set -e
+
+# # Apply database migrations
+# echo "Applying database migrations..."
+# python manage.py migrate --noinput --settings=config.settings.production
+
+
+
+# # Collect static files
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput --settings=config.settings.production
+
+# # Start Gunicorn
+# echo "Starting Gunicorn..."
+# exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
