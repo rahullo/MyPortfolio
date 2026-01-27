3e1a418daf59058a3a3690560ba3f39e16c1eada
Rahul Lohra <rahullohra987@gmail.com>
2026-01-27 13:27:12 +0530

    super user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,21 @@
+# #!/bin/sh
+
+# # Exit immediately if a command exits with a non-zero status
+# set -e
+
+# # Apply database migrations
+# echo "Applying database migrations..."
+# python manage.py migrate --noinput --settings=config.settings.production
+
+# # Collect static files
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput --settings=config.settings.production
+
+# # Start Gunicorn
+# echo "Starting Gunicorn..."
+# exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+
+
 #!/bin/sh
 
 # Exit immediately if a command exits with a non-zero status
@@ -6,6 +24,12 @@ set -e
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate --noinput --settings=config.settings.production
+
+# Create superuser ONLY if env vars exist
+if [ "$DJANGO_SU_NAME" ]; then
+  echo "Creating superuser (if not exists)..."
+  python scripts/create_superuser.py
+fi
 
 # Collect static files
 echo "Collecting static files..."
---
+++ scripts/create_superuser.py
@@ -0,0 +1,19 @@
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SU_NAME")
+email = os.environ.get("DJANGO_SU_EMAIL")
+password = os.environ.get("DJANGO_SU_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+        )
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
