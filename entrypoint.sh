091cc7f60de6215861d46e2d0905fd149bb43cb4
Rahul Lohra <rahullohra987@gmail.com>
2026-01-28 10:22:45 +0530

    Entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ set -e
 echo "Applying database migrations..."
 python manage.py migrate --noinput --settings=config.settings.production
 
-
+# Seed database (Create Admin)
+echo "Seeding database..."
+python seed_db.py
 
 # Collect static files
 echo "Collecting static files..."
@@ -16,23 +18,3 @@ python manage.py collectstatic --noinput --settings=config.settings.production
 # Start Gunicorn
 echo "Starting Gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
-
-
-# #!/bin/sh
-
-# # Exit immediately if a command exits with a non-zero status
-# set -e
-
-# # Apply database migrations
-# echo "Applying database migrations..."
-# python manage.py migrate --noinput --settings=config.settings.production
-
-
-
-# # Collect static files
-# echo "Collecting static files..."
-# python manage.py collectstatic --noinput --settings=config.settings.production
-
-# # Start Gunicorn
-# echo "Starting Gunicorn..."
-# exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
